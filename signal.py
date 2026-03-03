6ef9dd250a6f44498f923af9a31e964ee0fc80c6
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-03 07:30:45 +0530

    Add files via upload

---
+++ signal.py
@@ -0,0 +1,17 @@
+#signal 
+print("---The signal is------")
+
+choice=int(input("enter your choice(1-3):-"))
+if choice==1:
+    print("grren light")
+    print("goo..")
+elif choice==2:
+    print('yellow')
+    print('start ')
+
+elif choice==3:
+    print("red light")
+    print("..STOP..")
+
+else:
+    print("choose invalid choice choose coorect choice")
