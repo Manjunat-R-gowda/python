a1d17b0bc7097158b7a2da38c4dfbf58dadd1dd8
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-04 19:20:40 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+# Import QRCode from pyqrcode
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+
+s = "https://engineeringinkannada.in/"
+
+url = pyqrcode.create(s)
+
+url.svg("myqr.svg", scale = 8)
+
+url.png('myqr.png',scale = 6 )
