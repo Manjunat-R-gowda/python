2a37f4c3356535f57573642282c1e9ab24aa77c3
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-05 17:20:52 +0530

    Add files via upload

---
+++ match.py
@@ -0,0 +1,14 @@
+num=int(input("enter your number;-"))
+match num:
+    case 1:
+        print("one")
+    case 2:
+        print("two")
+    case 3:
+        print("three")
+    case 4:
+        print("four")
+    case 5:
+        print("five")
+    case _:
+        print("number is not in list please try again ! select another number")
