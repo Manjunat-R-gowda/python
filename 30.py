78385fa1b6f4d4828dc35b81fee2a0200bd6e2b0
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-08 21:04:05 +0530

    Add files via upload

---
+++ 30.py
@@ -0,0 +1,15 @@
+a=int(input("enter your number:-"))
+if a % 2==0:
+    print("Even number")
+
+else:
+    print("odd number")
+
+
+name="manjunath"
+print(len(name))
+
+i=0
+while i<=9:
+    i+=1
+    print(i)
