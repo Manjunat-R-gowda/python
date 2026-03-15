5c2209621b29bc52c11727f0171ca0f37778c496
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-15 21:23:36 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,23 @@
+#function practise simple program
+a=int(input("enter your  number:-"))
+b=int(input("enter your number:-"))
+
+
+def add(a,b):
+    print("Result:-",a+b)
+
+def sub(a,b):
+    print("Result:-",a-b)
+
+def mul(a,b):
+    print("Result:-",a*b)
+
+def div(a,b):
+    print("Result:-",a/b)
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+
+
